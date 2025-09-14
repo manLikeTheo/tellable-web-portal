2ec9eac01377e5ce18581f1ac0d145eb134c00aa
theophilus adewale <adewaletheophilus@gmail.com>
2025-09-14 04:26:56 +0100

    fix: disbale eslint during builds; remove deprecated swcMinify

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  swcMinify: true,
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
