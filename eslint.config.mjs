396fbb0e8b11fa8334557e244c6aebc0b8ccb524
theophilus adewale <adewaletheophilus@gmail.com>
2025-09-14 04:17:47 +0100

    fix: remove unused eslintrc.json; update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "warn"
+    }
+  }
 ];
 
 export default eslintConfig;
