07cfc5083038bd2b09d4f835eefca996addf46f8
theophilus adewale <adewaletheophilus@gmail.com>
2025-09-28 04:43:20 +0100

    add server side client

--- src/lib/supabase-server.ts
+++ src/lib/supabase-server.ts
@@ -1,14 +1,8 @@
 // lib/supabase-server.ts
-
 import { createClient } from "@supabase/supabase-js";
 
+//SERVER-SIDE use.
 const supabaseUrl = process.env.SUPABASE_URL || "";
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
-
-if (!supabaseUrl || !supabaseServiceKey) {
-  throw new Error(
-    "Missing SUPABASE_URL or SUPABASE_SERVICE_ROLE_KEY for server-side client."
-  );
-}
 
 export const supabaseServer = createClient(supabaseUrl, supabaseServiceKey);
